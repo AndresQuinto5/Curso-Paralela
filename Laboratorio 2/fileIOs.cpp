09132184aa592564bc7d1163929aed0bf783dd83
GuidoPadilla <sebaspadilla2010@hotmail.com>
2023-03-06 14:15:09 -0600

    codigo paralelo y secuencial

--- Laboratorio 2/fileIOs.cpp
+++ Laboratorio 2/fileIOs.cpp
@@ -8,34 +8,32 @@
 #include <stdio.h>
 using namespace std;
 using namespace std::chrono; // using chrono library
-void par_qsort(int *data, int lo, int hi)
+void quicksort(int *Array, int left, int right)
 {
-    if (lo > hi)
-        return;
-    int l = lo;
-    int h = hi;
-    int p = data[(hi + lo) / 2];
-    while (l <= h)
+    if (left < right)
     {
-        while ((data[l] - p) < 0)
-            l++;
-        while ((data[h] - p) > 0)
-            h--;
-        if (l <= h)
+        int pivot = Array[(left + right) / 2];
+        int i = left - 1;
+        int j = right + 1;
+        while (true)
         {
-            // swap
-            int tmp = data[l];
-            data[l] = data[h];
-            data[h] = tmp;
-            l++;
-            h--;
+            do
+            {
+                i++;
+            } while (Array[i] < pivot);
+            do
+            {
+                j--;
+            } while (Array[j] > pivot);
+            if (i >= j)
+            {
+                break;
+            }
+            swap(Array[i], Array[j]);
         }
+        quicksort(Array, left, j);
+        quicksort(Array, j + 1, right);
     }
-#pragma omp task if (h - lo > 150) mergeable
-    {
-        par_qsort(data, lo, h);
-    }
-    par_qsort(data, l, hi);
 }
 int main()
 {
@@ -45,12 +43,11 @@ int main()
     int *Array = new int[N];         // allocate memory in the heap
     ofstream outFile("numbers.csv"); // create output file stream
     double ti, tf, t;
-    double t_sec = 4.35;
-    int thread_count = 6;
+    double t_sec = 0.42;
+    int thread_count = 8;
     ti = omp_get_wtime();
     // generate N random numbers and write them to file
     srand(time(nullptr)); // set random seed based on current time
-#pragma omp parallel for num_threads(thread_count)
     for (int i = 0; i < N; i++)
     {
         Array[i] = rand() % 100; // generate random number between 0 and 99
@@ -72,17 +69,10 @@ int main()
         token = strtok(nullptr, delim);
     }
     inFile.close(); // close input file stream
-// sort numbers in ascending order
-#pragma omp parallel num_threads(thread_count)
-    {
-#pragma omp single
-        {
-            par_qsort(Array, 0, N - 1);
-        }
-    }
+    // sort numbers in ascending order
+    quicksort(Array, 0, N - 1);
     // write sorted numbers to file
     ofstream sortedFile("sorted_numbers.csv"); // create output file stream
-#pragma omp parallel for num_threads(thread_count)
     for (int i = 0; i < N; i++)
     {
         sortedFile << Array[i]; // write number to file
@@ -92,11 +82,7 @@ int main()
     sortedFile.close(); // close output file stream
     tf = omp_get_wtime();
     t = tf - ti;
-    printf("threads = %d\n", thread_count);
-    printf("tiempo secuencial 10000000 %f segundos\n", t_sec);
-    printf("tiempo %f segundos paralelo\n", t);
-    printf("SpeedUp %f\n", t_sec / t);
-    printf("Eficiencia %f\n", (t_sec / t) / thread_count);
+    printf("tiempo secuencial %f segundos\n", t);
     delete[] Array; // free memory allocated in the heap
     return 0;
 }
